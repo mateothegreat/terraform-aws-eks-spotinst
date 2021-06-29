7e2cac34612dbabde49d4615a5b380a80697faad
Matthew Davis <matthew@matthewdavis.io>
2021-06-28 21:28:05 -0400

    bump

--- modules/ocean-eks/spotio-notifications.tf
+++ modules/ocean-eks/spotio-notifications.tf
@@ -2,7 +2,7 @@ resource "spotinst_subscription" "default-subscription" {
 
     count = var.notification_sqs_arn != null ? length(var.spot_subscription_events) : 0
 
-    resource_id = spotinst_ocean_aws.this.id
+    resource_id = module.k8s-ocean.id
     event_type  = var.spot_subscription_events[ count.index ]
     protocol    = "aws-sns"
     endpoint    = var.notification_sqs_arn
--- modules/ocean-eks/spotio-ocean-controller.tf
+++ modules/ocean-eks/spotio-ocean-controller.tf
@@ -1,24 +1,24 @@
+##
+## Deploy the ocean controller pod.
+##
+#module "ocean-controller" {
 #
-# Deploy the ocean controller pod.
+#    depends_on = [
 #
-module "ocean-controller" {
-
-    depends_on = [
-
-        module.vpc,
-
-        aws_route.peering_forward,
-        aws_route.peering_backward,
-        aws_route.peering_backward_private_subnets,
-        aws_route.peering_backward_public_subnets
-
-    ]
-
-    source = "../ocean-controller"
-
-    spotinst_token     = var.spotinst_token
-    spotinst_account   = var.spotinst_account
-    cluster_identifier = var.cluster_name
-    node_selector      = var.controller_node_selector
-
-}
+#        module.vpc,
+#
+#        aws_route.peering_forward,
+#        aws_route.peering_backward,
+#        aws_route.peering_backward_private_subnets,
+#        aws_route.peering_backward_public_subnets
+#
+#    ]
+#
+#    source = "../ocean-controller"
+#
+#    spotinst_token     = var.spotinst_token
+#    spotinst_account   = var.spotinst_account
+#    cluster_identifier = var.cluster_name
+#    node_selector      = var.controller_node_selector
+#
+#}
