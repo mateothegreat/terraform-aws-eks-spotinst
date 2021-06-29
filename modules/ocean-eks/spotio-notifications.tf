956786f51b5432557c265b8edf72e9345b593609
Matthew Davis <matthew@matthewdavis.io>
2021-06-28 22:35:18 -0400

    bump

--- modules/ocean-eks/spotio-notifications.tf
+++ modules/ocean-eks/spotio-notifications.tf
@@ -1,10 +1,10 @@
-resource "spotinst_subscription" "default-subscription" {
-
-    count = var.notification_sqs_arn != null ? length(var.spot_subscription_events) : 0
-
-    resource_id = module.k8s-ocean.ocean_id
-    event_type  = var.spot_subscription_events[ count.index ]
-    protocol    = "aws-sns"
-    endpoint    = var.notification_sqs_arn
-
-}
+#resource "spotinst_subscription" "default-subscription" {
+#
+#    count = var.notification_sqs_arn != null ? length(var.spot_subscription_events) : 0
+#
+#    resource_id = module.k8s-ocean.ocean_id
+#    event_type  = var.spot_subscription_events[ count.index ]
+#    protocol    = "aws-sns"
+#    endpoint    = var.notification_sqs_arn
+#
+#}
