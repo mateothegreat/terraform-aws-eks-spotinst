6e17a61e4cc705b36074acb2ce61efb1c045a8bd
Matthew Davis <matthew@matthewdavis.io>
2022-11-12 19:40:52 -0500

    bump

--- test/test.tf
+++ test/test.tf
@@ -1,18 +1,11 @@
-locals {
-
-    cluster_name = "mlfabric-sandbox-testing-1"
-    tags         = {}
-
-}
-
 variable "spotinst_token" {}
 
 module "test" {
 
     source = "../"
 
-    dest_aws_profile_name            = "mlfabric-sandbox"
-    spotinst_account                 = "act-44600505"
+    dest_aws_profile_name            = ""
+    spotinst_account                 = ""
     spotinst_token                   = var.spotinst_token
     cluster_name                     = "matthew-1"
     cluster_version                  = "1.20"
@@ -23,20 +16,19 @@ module "test" {
     vpc_cidr                         = "172.200.4.0/23"
     private_subnets                  = [ "172.200.4.0/25", "172.200.4.128/25" ]
     public_subnets                   = [ "172.200.5.0/25", "172.200.5.128/25" ]
-    peering_name                     = "${local.cluster_name }-to-devops-vpn"
-    peering_owner_id                 = "881029454603"
+    peering_name                     = "dev-to-devops-vpn"
+    peering_owner_id                 = ""
     peering_orginator_route_table_id = "rtb-0c3f673158e4eeec0"
     peering_originator_cidr          = "8.0.0.224/32"
     peering_receiver_vpc_id          = "vpc-09dca0f5229137c2b"
-    peering_receiver_profile         = "mlfabric"
+    peering_receiver_profile         = ""
     peering_receiver_region          = "us-east-1"
     notification_sqs_arn             = null
     cluster_endpoint_private_access  = true
     cluster_endpoint_public_access   = false
     controller_node_selector         = null
     ami_id                           = "ami-0d6c8b2a8562eba37"
-
-    tags = local.tags
+    tags                             = {}
 
     launch_specs = [
 
